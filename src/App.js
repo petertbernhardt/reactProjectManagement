1085516fe5c04d2cdf4c9773578b0b3bea3fa867
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2017-12-06 10:48:13 -0800

    Cleaned up code and made it more component-based

--- src/App.js
+++ src/App.js
@@ -6,6 +6,30 @@ import { Navbar, Nav, NavItem, MenuItem, Jumbotron, Button } from 'react-bootstr
 
 const Home = () => <h1>Home</h1>;
 
+const Navigation = () => (  
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to={'/'} activeClassName="active">Home</Link>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <NavItem>
+        <Link to={'/add'} activeClassName="active">Add a Project</Link>
+      </NavItem>
+      <NavItem>
+        <Link to={'/view'} activeClassName="active">View a Project</Link>
+      </NavItem>
+      <NavItem>
+        <Link to={'/edit'} activeClassName="active">Edit a Project</Link>
+      </NavItem>
+      <NavItem>
+        <Link to={'/export'} activeClassName="active">Export all Projects</Link>
+      </NavItem>
+    </Nav>
+  </Navbar>
+);
+
 const Add = () => <h1>Add a Project</h1>;
 
 const View = () => <h1>View a Project</h1>;
@@ -14,41 +38,25 @@ const Edit = () => <h1>Edit a Project</h1>;
 
 const Export = () => <h1>Export all Projects</h1>;
 
+const Content = () => (
+  <div className="content">
+    <Route exact path="/" component={Home} />
+    <Route exact path="/add" component={Add} />
+    <Route exact path="/view" component={View} />
+    <Route exact path="/edit" component={Edit} />
+    <Route exact path="/export" component={Export} />
+  </div>
+);
+
 const App = () => (
+  <div>
+    <Router>
       <div>
-        <Router>
-          <div>
-            <Navbar>
-              <Navbar.Header>
-                <Navbar.Brand>
-                  <Link to={'/'} activeClassName="active">Home</Link>
-                </Navbar.Brand>
-              </Navbar.Header>
-              <Nav>
-                <NavItem>
-                  <Link to={'/add'} activeClassName="active">Add a Project</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to={'/view'} activeClassName="active">View a Project</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to={'/edit'} activeClassName="active">Edit a Project</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to={'/export'} activeClassName="active">Export all Projects</Link>
-                </NavItem>
-              </Nav>
-            </Navbar>
-            <div className="content">
-              <Route exact path="/" component={Home} />
-              <Route exact path="/add" component={Add} />
-              <Route exact path="/view" component={View} />
-              <Route exact path="/edit" component={Edit} />
-              <Route exact path="/export" component={Export} />
-            </div>
-          </div>
-        </Router>
+        <Navigation />
+        <Content />
       </div>
+    </Router>
+  </div>
 );
 
 export default App
