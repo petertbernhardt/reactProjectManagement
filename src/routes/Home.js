9f3e9c5e41751af7d54313ca734798ef543b7cb6
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2017-12-06 17:13:35 -0800

    Fixed styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,25 +3,31 @@ import React from 'react';
 const Home = () => (
 	<div className="container">
 		<h1>Home</h1>
-		<h3>Projects</h3>
 		<div className="col-md-12">
-			<ul className="list col-md-4">
-				<li>Go to Mars</li>
-				<li>World Peace</li>
-				<li>Half Life 3</li>
-			</ul>
-			<h3>Managers</h3>
-			<ul className="list col-md-4">
-				<li>Elon Musk</li>
-				<li>Gabe Newell</li>
-				<li>Emperor of Mankind</li>
-			</ul>
-			<h3>Workers</h3>
-			<ul className="list col-md-4">
-				<li>Bob</li>
-				<li>Scully</li>
-				<li>Hitchcock</li>
-			</ul>
+			<div className="col-md-4 col-sm-6 col-xs-12">
+				<h3>Projects</h3>
+				<ul className="list">
+					<li>Go to Mars</li>
+					<li>World Peace</li>
+					<li>Half Life 3</li>
+				</ul>
+			</div>
+			<div className="col-md-4 col-sm-6 col-xs-12">
+				<h3>Managers</h3>
+				<ul className="list">
+					<li>Elon Musk</li>
+					<li>Gabe Newell</li>
+					<li>Emperor of Mankind</li>
+				</ul>
+			</div>
+			<div className="col-md-4 col-sm-6 col-xs-12">
+				<h3>Workers</h3>
+				<ul className="list">
+					<li>Bob</li>
+					<li>Scully</li>
+					<li>Hitchcock</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 );
